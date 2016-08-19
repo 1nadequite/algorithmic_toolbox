014f4ead52c83321dc47f9d59c3645cfcab4661b
1nadequite <yaretsaa@gmail.com>
2016-08-19 23:07:58 +0300

    organizing lottery

--- w4/lottery.cpp
+++ w4/lottery.cpp
@@ -2,64 +2,74 @@
 // each point, the number of segments that contain this point.
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
+typedef pair<int, int> pii;
 
-int partition(vector<pair<int, int>>& a, int left, int right) {
-  int i = left, j = right;
-  int pivot = a[left + (right - left) / 2].first;
-
-  while (i <= j) {
-    while (a[i].first < pivot) i++;
-    while (a[j].first > pivot) j--;
-    if (i <= j) {
-      swap(a[i], a[j]);
+// merge sort for vector<int, int>
+vector<pii> merge(const vector<pii>& left, const vector<pii>& right) {
+  vector<pii> res;
+  int i = 0, j = 0;
+  while (i < left.size() && j < right.size()) {
+    if (left[i] < right[j]) {
+      res.push_back(left[i]);
       i++;
-      j--;
+    } else {
+      res.push_back(right[j]);
+      j++;
     }
   }
-  return i;
-}
-
-void quick2_sort(vector<pair<int, int>>& a, int left, int right) {
-  if (left >= right) return;
-  int index = partition(a, left, right);
-  if (left < index - 1) quick2_sort(a, left, index - 1);
-  if (index < right) quick2_sort(a, index, right);
-}
-
-int binary_search(const vector<pair<int, int>>& a, int x) {
-  int l = 0, r = a.size() - 1;
-  while (r - l > 1) {
-    int mid = (l + r) / 2;
-    if (x < a[mid].first) r = mid;
-    else l = mid;
+  while (i < left.size()) {
+    res.push_back(left[i]);
+    i++;
   }
-  int ans = 0;
-  if (a[l].first <= x && x <= a[l].second) ans++;
-  else if (a[r].first <= x && x <= a[r].second) ans++;
-  return ans;
+  while (j < right.size()) {
+    res.push_back(right[j]);
+    j++;
+  }
+  return res;
+}
+
+vector<pii> merge_sort(const vector<pii>& a) {
+  int n = a.size();
+  if (n == 1) return a;
+  int m = n / 2;
+  vector<pii> left_side = {a.begin(), a.begin() + m};
+  vector<pii> right_side = {a.begin() + m, a.end()};
+  vector<pii> left = merge_sort(left_side);
+  vector<pii> right = merge_sort(right_side);
+  return merge(left, right);
 }
 
 int main() {
   int n, m; cin >> n >> m;
-  vector<pair<int, int>> a(n);
-  vector<int> b(m);
+  vector<pii> a;
+  map<int, int> pos;
   for (int i = 0; i < n; ++i) {
     int x, y; cin >> x >> y;
-    a[i] = {x, y};
+    a.push_back({x, 0});
+    a.push_back({y, 2});
   }
-  for (int i = 0; i < m; ++i) cin >> b[i];
+  for (int i = 0; i < m; ++i) {
+    int x; cin >> x;
+    a.push_back({x, 1});
+    pos[x] = i;
+  }
 
-  vector<int> res(m);
-  quick2_sort(a, 0, n - 1);
-  /*for (int i = 0; i < m; ++i)
-    res[i] += binary_search(a, b[i]);
-  for (auto x: res)
+  vector<pii> sort_a;
+  sort_a = merge_sort(a);
+
+  vector<int> ans(m);
+  int cnt = 0;
+  for (auto x: sort_a) {
+    if (x.second == 0) cnt++;
+    else if (x.second == 2) cnt--;
+    else ans[pos[x.first]] = cnt;
+  }
+  for (auto x: ans)
     cout << x << ' ';
-  cout << endl;*/
-  for (auto x: a)
-    cout << x.first << ' ' << x.second << endl;
+  cout << endl;
 
   return 0;
 }
